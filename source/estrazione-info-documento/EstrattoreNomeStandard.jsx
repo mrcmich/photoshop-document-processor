9be88824db3b5f78f431ce7c525007facae588cd
Marco Michelini <marco.michelini@protonmail.com>
2021-09-28 18:22:00 +0200

    Eliminate variabili locali superflue dal metodo estraiInfo(documento) di EstrattoreNomeStandard.

--- source/estrazione-info-documento/EstrattoreNomeStandard.jsx
+++ source/estrazione-info-documento/EstrattoreNomeStandard.jsx
@@ -4,9 +4,6 @@ function EstrattoreNomeStandard() {
     this.__proto__ = EstrattoreInfoAstratto;
 
     this.estraiInfo = function(documento) {
-        var nomeCompletoDocumento;
-        var posizioneSeparatoreFormato;
-
         if (documento == undefined) {
             throw new Error(
                 "Invocazione del metodo estraiInfo(documento) di EstrattoreNomeStandard " +
