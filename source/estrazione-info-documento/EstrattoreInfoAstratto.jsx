a84f55a97370d8797c0484d32639652492b87866
Marco Michelini <marco.michelini@protonmail.com>
2021-09-28 12:55:18 +0200

    Aggiunto EstrattoreInfoAstratto.jsx.

---
+++ source/estrazione-info-documento/EstrattoreInfoAstratto.jsx
@@ -0,0 +1,6 @@
+var EstrattoreInfoAstratto = {
+    estraiInfo: function(documento) {
+        throw new SyntaxError("Invocazione del metodo astratto estraiInfo(documento) di EstrattoreInfoAstratto.");
+    }
+    
+};
