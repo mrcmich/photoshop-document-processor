0b49bf8795771e6ee2c35b302f08186d59e92359
Marco Michelini <marco.michelini@protonmail.com>
2021-09-28 13:01:24 +0200

    Aggiunto PosizionatoreLivelloAstratto.jsx.

---
+++ source/posizionamento-livello/PosizionatoreLivelloAstratto.jsx
@@ -0,0 +1,34 @@
+var PosizionatoreLivelloAstratto = {
+    REGIONE_SINISTRA = 0,
+    REGIONE_CENTRALE = 1,
+    REGIONE_DESTRA = 2,
+
+    rilevaRegione: function(livello, documento) {
+        throw new SyntaxError("Invocazione del metodo astratto rilevaRegione(livello, documento) di PosizionatoreLivelloAstratto.");
+    },
+
+    riposizionaLivello: function(livello, documento, regione, margine) {
+        throw new SyntaxError(`
+            Invocazione del metodo astratto riposizionaLivello(livello, documento, regione, margine) di PosizionatoreLivelloAstratto.
+        `);
+    }
+
+};
+
+Object.defineProperty(PosizionatoreLivelloAstratto, "REGIONE_SINISTRA", {
+    "writable": false,
+    "enumerable": false,
+    "configurable": false
+});
+
+Object.defineProperty(PosizionatoreLivelloAstratto, "REGIONE_CENTRALE", {
+    "writable": false,
+    "enumerable": false,
+    "configurable": false
+});
+
+Object.defineProperty(PosizionatoreLivelloAstratto, "REGIONE_DESTRA", {
+    "writable": false,
+    "enumerable": false,
+    "configurable": false
+});
