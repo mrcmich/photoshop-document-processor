b381a96c2d26dde5e7d8512facce4e926888bf2d
Marco Michelini <marco.michelini@protonmail.com>
2021-09-30 08:58:15 +0200

    Aggiunto TabellaToniAstratta.jsx.

---
+++ source/tonalizzazione/TabellaToniAstratta.jsx
@@ -0,0 +1,20 @@
+var TabellaToniAstratta = {
+    aggiungiTono: function(documento, tono) {
+        throw new Error(
+            "Invocazione del metodo astratto aggiungiTono(documento, tono) di TabellaToniAstratta."
+        );
+    },
+
+    calcolaTonoMedio: function() {
+         throw new Error(
+            "Invocazione del metodo astratto calcolaTonoMedio() di TabellaToniAstratta."
+        );
+    },
+
+    toString: function() {
+         throw new Error(
+            "Invocazione del metodo astratto toString() di TabellaToniAstratta."
+        );
+    }
+
+};
