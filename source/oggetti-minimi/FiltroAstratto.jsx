4a2609e21e4860abdb47d0860abdbba3fc30baf7
Marco Michelini <marco.michelini@protonmail.com>
2021-10-16 19:25:22 +0200

    Aggiunta documentazione FiltroAstratto.

--- source/oggetti-minimi/FiltroAstratto.jsx
+++ source/oggetti-minimi/FiltroAstratto.jsx
@@ -1,4 +1,17 @@
+/**
+* Oggetto JavaScript che rappresenta l'astrazione di un filtro. 
+* Deve essere settato come prototipo di un filtro, il quale deve implementarne i metodi.
+* Vedi sorgente per i dettagli sui metodi da implementare.
+*/
 var FiltroAstratto = {
+
+    /**
+    * Metodo senza implementazione per l'esecuzione del filtro.
+    * Il filtro si applica all'array' di documenti passato come parametro.
+    * @abstract
+    * @param {Array} documenti - i documenti che il filtro deve processare.
+    * @throws {Error}
+    */
     esegui: function(documenti) {
         throw new Error(
             "Invocazione del metodo astratto esegui() di FiltroAstratto."
