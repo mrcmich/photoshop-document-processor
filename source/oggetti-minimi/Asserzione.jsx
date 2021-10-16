49c56c97f3b141749b0046c1a258156a332af9a4
Marco Michelini <marco.michelini@protonmail.com>
2021-10-16 19:33:32 +0200

    Aggiunta documentazione Asserzione.

--- source/oggetti-minimi/Asserzione.jsx
+++ source/oggetti-minimi/Asserzione.jsx
@@ -1,3 +1,12 @@
+/**
+* Funzione che lancia un errore se la condizione fornita come parametro
+* non è verificata, documentandolo in maniera opportuna.
+* @param {boolean} condizione - la condizione che viene valutata.
+* @param {string} metodo - il nome del metodo in cui la condizione è valutata.
+* @param {string} oggetto - il nome dell'oggetto in cui la condizione è valutata.
+* @param {string} descrizione - la descrizione dell'errore che deve essere generato se la condizione non è verificata.
+* @throws Lancia un errore se la condizione non è verificata.
+*/
 function asserzione(condizione, metodo, oggetto, descrizione) {
     if (!condizione) {
         throw new Error(
