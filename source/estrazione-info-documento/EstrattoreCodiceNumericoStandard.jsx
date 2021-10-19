bda31a964923a57d4880ae8fd04e8070aa8c9b44
Marco Michelini <marco.michelini@protonmail.com>
2021-10-19 20:30:32 +0200

    Aggiunta verifica formato nome documento al metodo estraiInfo(documento) di EstrattoreCodiceNumericoStandard.

--- source/estrazione-info-documento/EstrattoreCodiceNumericoStandard.jsx
+++ source/estrazione-info-documento/EstrattoreCodiceNumericoStandard.jsx
@@ -15,10 +15,12 @@ function EstrattoreCodiceNumericoStandard() {
     * Assume che il nome del documento sia in formato standard, del tipo "CN_STR.FFF" (con CN codice numerico intero, STR stringa costituita
     * di una o più parole separate dal carattere underscore e FFF estensione del documento).
     * @param {Document} documento - il documento di cui si vuole estrarre il codice numerico.
-    * @throws Lancia un errore se il parametro passato è null o undefined.
+    * @throws Lancia un errore se il parametro passato è null o undefined, oppure se il nome del documento non è in formato standard.
     * @returns {string}
     */
     this.estraiInfo = function(documento) {
+        var partiNomeDocumento;
+
         asserzione(
             documento != undefined, 
             "estraiInfo(documento)", 
@@ -26,7 +28,16 @@ function EstrattoreCodiceNumericoStandard() {
             "documento null o undefined."
         );
 
-        return (documento.name.split("_"))[0];
+        partiNomeDocumento = documento.name.split("_");
+
+        asserzione(
+            partiNomeDocumento.length >= 2, 
+            "estraiInfo(documento)", 
+            "EstrattoreCodiceNumericoStandard", 
+            "documento con nome non in formato standard."
+        );
+
+        return partiNomeDocumento[0];
     };
 
 }
