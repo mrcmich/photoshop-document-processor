63b58ac56d93b43a07669ff2bf01a41b188e6de1
Marco Michelini <marco.michelini@protonmail.com>
2021-10-19 19:01:32 +0200

    Aggiunta verifica formato nome documento al metodo estraiInfo(documento) di EstrattoreNomeStandard.

--- source/estrazione-info-documento/EstrattoreNomeStandard.jsx
+++ source/estrazione-info-documento/EstrattoreNomeStandard.jsx
@@ -15,10 +15,12 @@ function EstrattoreNomeStandard() {
     * Assume che il nome sia in formato standard, del tipo "CN_STR.FFF" (con CN codice numerico intero, STR stringa costituita
     * di una o più parole separate dal carattere underscore e FFF estensione del documento).
     * @param {Document} documento - il documento di cui si vuole estrarre il nome.
-    * @throws Lancia un errore se il parametro passato è null o undefined.
+    * @throws Lancia un errore se il parametro passato è null o undefined, oppure se il nome del documento non è in formato standard.
     * @returns {string}
     */
     this.estraiInfo = function(documento) {
+        var partiNomeDocumento;
+
         asserzione(
             documento != undefined, 
             "estraiInfo(documento)", 
@@ -26,7 +28,16 @@ function EstrattoreNomeStandard() {
             "documento null o undefined."
         );
 
-        return (documento.name.split("."))[0];
+        partiNomeDocumento = documento.name.split(".");
+
+        asserzione(
+            partiNomeDocumento.length == 2, 
+            "estraiInfo(documento)", 
+            "EstrattoreNomeStandard", 
+            "documento con nome non in formato standard."
+        );
+
+        return partiNomeDocumento[0];
     };
 
 }
