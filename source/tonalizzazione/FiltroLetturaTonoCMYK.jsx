4b511821840094935c9ba196e67a10fc50b92967
Marco Michelini <marco.michelini@protonmail.com>
2021-10-27 10:59:30 +0200

    Completata documentazione.

--- source/tonalizzazione/FiltroLetturaTonoCMYK.jsx
+++ source/tonalizzazione/FiltroLetturaTonoCMYK.jsx
@@ -15,9 +15,43 @@
 function FiltroLetturaTonoCMYK(tabellaToni, scrittoreTabellaToni) {
     this.__proto__ = FiltroLetturaTonoAstratto;
 
-    //
+    /**
+    * Array contenente i documenti che il filtro va a processare.
+    * @type {Array}
+    * @protected
+    */
+    this._documenti = null;
+
+    /**
+    * Array contenente il nome dei documenti con tono che il filtro non è in grado di gestire,
+    * ovvero un tono per cui uno o più canali si trovino allo 0%.
+    * @type {Array}
+    * @protected
+    */
     this._documentiConTonoNonValido = [];
 
+    /**
+    * Attributo che rappresenta la tabella dei toni che il filtro va a compilare con 
+    * il tono dei documenti ad esso passati.
+    * @type {TabellaToniAstratta}
+    * @protected
+    */
+    this._tabellaToni = null;
+
+    /**
+    * Oggetto deputato al salvataggio su file della tabella dei toni compilata dal filtro.
+    * @type {ScrittoreFileAstratto}
+    * @protected
+    */
+    this._scrittoreTabellaToni = null;
+
+    /**
+    * Metodo per la validazione dell'array di documenti passato come parametro.
+    * Sono considerati validi solo documenti con metodo colore CMYK.
+    * Ritorna un array contenente il nome dei documenti non validi, o l'array vuoto se tutti i documenti sono validi.
+    * @param {Array} documenti - array dei documenti da validare.
+    * @returns {Array}
+    */
     this.validaDocumenti = function(documenti) {
         var documentiNonValidi = [];
 
@@ -30,14 +64,28 @@ function FiltroLetturaTonoCMYK(tabellaToni, scrittoreTabellaToni) {
         return documentiNonValidi;
     };
 
+    /**
+    * Metodo getter per l'attributo _documentiConTonoNonValido.
+    * @returns {Array}
+    */
     this.leggiDocumentiConTonoNonValido = function() {
         return this._documentiConTonoNonValido;
     };
 
+    /**
+    * Metodo getter per l'attributo _tabellaToni.
+    * @returns {TabellaToniAstratta}
+    */
     this.leggiTabellaToni = function() {
         return this._tabellaToni;
     };
 
+    /**
+    * Metodo setter per l'attributo _documenti.
+    * @param {Array} documenti - documenti che il filtro deve processare.
+    * @throws Lancia un errore se il parametro passato è null o undefined, o se contiene documenti non validi.
+    * @returns {undefined}
+    */
     this.settaDocumenti = function(documenti) {
         var documentiNonValidi;
 
@@ -63,7 +111,6 @@ function FiltroLetturaTonoCMYK(tabellaToni, scrittoreTabellaToni) {
     /**
     * Metodo per l'esecuzione del filtro lettura tono.
     * Il filtro si applica all'array di documenti passato come parametro.
-    * @abstract
     * @param {Array} documenti - i documenti che il filtro deve processare.
     * @returns {undefined}
     */
@@ -96,6 +143,14 @@ function FiltroLetturaTonoCMYK(tabellaToni, scrittoreTabellaToni) {
 
     };
 
+    /**
+    * Metodo per la rilevazione della quantità percentuale del canale specificato, valutata sul livello
+    * passato come parametro. Ritorna la quantità percentuale letta, arrotondata all'intero più vicino.
+    * @param {ArtLayer} livello - il livello del documento rispetto al quale viene fatta la rilevazione, tipicamente una tinta piatta.
+    * @param {ColorSampler} campionatoreColore - oggetto deputato alla lettura della quantità percentuale del canale.
+    * @param {string} canale - il nome del canale oggetto della rilevazione, uno dei quattro canali CMYK (cyan, magenta, yellow, black).
+    * @returns {number}
+    */
     this.rilevaPercentualeCanale = function(livello, campionatoreColore, canale) {
         var coloreCampionato = campionatoreColore.color.cmyk;
 
@@ -105,7 +160,6 @@ function FiltroLetturaTonoCMYK(tabellaToni, scrittoreTabellaToni) {
     /**
     * Metodo per la rilevazione del tono di un documento, valutato sul livello
     * di tale documento passato come parametro.
-    * @abstract
     * @param {ArtLayer} livello - il livello del documento rispetto al quale il tono viene rilevato, tipicamente una tinta piatta.
     * @param {ColorSampler} campionatoreColore - oggetto deputato alla lettura delle quantità percentuali dei canali del documento.
     * @returns {SolidColor}
@@ -121,6 +175,13 @@ function FiltroLetturaTonoCMYK(tabellaToni, scrittoreTabellaToni) {
         return tono;
     };
 
+    /**
+    * Metodo per la validazione del tono passato come parametro.
+    * Ritorna true se il tono passato è valido (cioè non è null/undefined e non contiene componenti pari allo 0%),
+    * oppure false in caso contrario.
+    * @param {SolidColor} tono - il tono da validare.
+    * @returns {boolean}
+    */
     this.validaTono = function(tono) {
          if (
             tono == undefined ||
@@ -135,6 +196,12 @@ function FiltroLetturaTonoCMYK(tabellaToni, scrittoreTabellaToni) {
         return true;
     };
 
+    /**
+    * Metodo per la compilazione della tabella dei toni del filtro.
+    * Per ogni documento passato al filtro, viene rilevato il tono del documento, e questo tono
+    * viene aggiunto alla tabella.
+    * @returns {undefined}
+    */
     this._compilaTabellaToni = function() {
         var tonoCorrente;
         var livelloRiferimento;
@@ -160,6 +227,12 @@ function FiltroLetturaTonoCMYK(tabellaToni, scrittoreTabellaToni) {
         }
     };
 
+    /**
+    * Metodo setter per l'attributo _tabellaToni.
+    * @throws Lancia un errore se il parametro passato è null o undefined.
+    * @param {TabellaToniAstratta} tabellaToni - la tabella dei toni dei documenti, compilata dal filtro lettura tono.
+    * @returns {undefined}
+    */
     this.settaTabellaToni = function(tabellaToni) {
         asserzione(
             tabellaToni != undefined, 
@@ -171,6 +244,12 @@ function FiltroLetturaTonoCMYK(tabellaToni, scrittoreTabellaToni) {
         this._tabellaToni = tabellaToni;
     };
 
+    /**
+    * Metodo setter per l'attributo _scrittoreTabellaToni.
+    * @throws Lancia un errore se il parametro passato è null o undefined.
+    * @param {ScrittoreFileAstratto} scrittoreTabellaToni - oggetto deputato al salvataggio della tabella dei toni su file.
+    * @returns {undefined}
+    */
     this.settaScrittoreTabellaToni = function(scrittoreTabellaToni) {
         asserzione(
             scrittoreTabellaToni != undefined, 
