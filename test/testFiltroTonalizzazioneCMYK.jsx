55f3161d493c996b3e1bbebbcfffd766ece09561
Marco Michelini <marco.michelini@protonmail.com>
2021-10-30 10:15:37 +0200

    Aggiunti test FiltroTonalizzazioneCMYK.

---
+++ test/testFiltroTonalizzazioneCMYK.jsx
@@ -0,0 +1,6 @@
+//@include "jasmine/boot.jsx";
+//@include "spec/filtroTonalizzazioneCMYK.spec"
+
+// ATTENZIONE:
+// Aprire i documenti in test/docs/FiltroTonalizzazioneCMYK/dopo in Photoshop prima dell'esecuzione del test!!!
+runJasmine();
